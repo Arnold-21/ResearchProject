eb3c26f995e45fb6c3fbecbc7617afb0ce839934
916_Sali_Arnold <91786230+Arnold-21@users.noreply.github.com>
2023-11-16 11:11:44 +0200

    Start of doing the environment to run the experiments

--- models.py
+++ models.py
@@ -4,9 +4,13 @@ from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Activa
 from tensorflow.keras.losses import sparse_categorical_crossentropy
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import tensorflow_hub as hub
-import numpy as np
-import matplotlib.pyplot as plt
+import time
+import os
+
+#Toggling gpu or cpu use
+gpuUse = True
+if not gpuUse:
+     os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 #Training variables
 imgSize = 224
@@ -15,10 +19,10 @@ epochs = 5
 trainPath = "./images/asl_alphabet_train/asl_alphabet_train"
 testPath = "./images/asl_alphabet_test/asl_alphabet_test"
 
-#Handling gpu usage
-# physicalDevices = tf.config.experimental.list_physical_devices('GPU')
-# if len(physicalDevices) != 0:
-#     tf.config.experimental.set_memory_growth(physicalDevices[0], True)
+#Handling gpu usage by setting memory growth
+physicalDevices = tf.config.experimental.list_physical_devices('GPU')
+if len(physicalDevices) != 0:
+    tf.config.experimental.set_memory_growth(physicalDevices[0], True)
 
 #Data generators for augmentation and rescaling
 trainDataAugment = ImageDataGenerator(preprocessing_function=tf.keras.applications.mobilenet.preprocess_input,
@@ -41,20 +45,52 @@ test_dataset = testDataAugment.flow_from_directory(testPath,
      target_size=(imgSize, imgSize),
      batch_size=batchSize)
 
-#Getting the pretrained mobilnetV2 model from google repo
-url = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/4"
-download_model = hub.KerasLayer(url,input_shape=(imgSize,imgSize,3))
-mobile = Sequential([
-     download_model,
-     Dense(29),
-     Activation("softmax")
-])
+#Reading the the experiment parameters from an input file
+fin = open("experiments.in", "r")
+for line in fin.readlines():
+     tokens = line.split(" ")
+     x = int(tokens[0])
+     y = int(tokens[1])
+     z = int(tokens[2])
+     print(x, " ", y, " ", z)
 
-#Compiling
-mobile.compile(optimizer=Adam(1e-3), loss="categorical_crossentropy", metrics=['accuracy'])
+fin.close()
 
-#training model
-mobile.fit(train_dataset, batch_size=batchSize, epochs=epochs)
+# #Getting the mobilenet model from the keras collections
+# mobilenet = tf.keras.applications.mobilenet.MobileNet()
 
-#evaluating model
+# #Switching last layers to a dense layer with the activation function for the asl dataset
+# switchLastLayers = 23
+# pretrainedLayers = mobilenet.layers[-switchLastLayers].output
+# reshapeOutput = tf.keras.layers.Reshape((512, ))(tf.keras.layers.GlobalAveragePooling2D()(pretrainedLayers))
+# output = Dense(units=29, activation="softmax")(reshapeOutput)
+
+# #Getting the final model from the pretrained mobilenet model
+# mobile = tf.keras.Model(inputs = mobilenet.input, outputs = output)
+
+# #Fixing the first couple layers for training
+# trainableLayerNumber = 1
+# for layer in mobile.layers[:-trainableLayerNumber]:
+#     layer.trainable = False
+
+
+# #Compiling
+# mobile.compile(optimizer=Adam(1e-3), loss="categorical_crossentropy", metrics=['accuracy'])
+
+# #training model
+# mobile.fit(train_dataset, batch_size=batchSize, epochs=epochs)
+
+# # evaluating model
 # mobile.evaluate(test_dataset)
+
+# #Calculating the elapsed time for predicitons
+# #Making an initial prediction for the tf.function to initialize
+# mobile.predict(x=train_dataset[0][0])
+
+# start = time.time()
+# mobile.predict(x=train_dataset[1][0])
+# mobile.predict(x=train_dataset[2][0])
+# mobile.predict(x=train_dataset[3][0])
+# mobile.predict(x=train_dataset[4][0])
+# end = time.time()
+# print("Average time: ", (end-start)/5, " s")
