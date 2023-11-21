d4c0d0dbe341081512cae25b396fa7d84771c15d
916_Sali_Arnold <91786230+Arnold-21@users.noreply.github.com>
2023-11-21 19:45:17 +0200

    Running the experiment and 1-2 for the docs written

--- models.py
+++ models.py
@@ -14,8 +14,8 @@ if not gpuUse:
 
 #Training variables
 imgSize = 224
-batchSize = 32
-epochs = 5
+batchSize = 64
+epochs = 7
 trainPath = "./images/asl_alphabet_train/asl_alphabet_train"
 testPath = "./images/asl_alphabet_test/asl_alphabet_test"
 
@@ -47,50 +47,65 @@ test_dataset = testDataAugment.flow_from_directory(testPath,
 
 #Reading the the experiment parameters from an input file
 fin = open("experiments.in", "r")
+fileCounter = 0
+experimentSkip = 5
+
 for line in fin.readlines():
+     #Increment the counter for the output
+     fileCounter += 1
+
+     #Skipping experiments that were run already
+     if fileCounter <= experimentSkip:
+          continue
+
+     #Opening the file to store the results
+     fout = open("experiments" + str(fileCounter) +".out", "w")
+
+     #Reading the values from the input file
      tokens = line.split(" ")
-     x = int(tokens[0])
-     y = int(tokens[1])
-     z = int(tokens[2])
-     print(x, " ", y, " ", z)
+     switchLastLayers = int(tokens[0])
+     reshapeSize = int(tokens[1])
+     trainableLayerNumber = int(tokens[2])
+
+     #Getting the mobilenet model from the keras collections
+     mobilenet = tf.keras.applications.mobilenet.MobileNet()
+
+     #Switching last layers to a dense layer with the activation function for the asl dataset
+     pretrainedLayers = mobilenet.layers[-switchLastLayers].output
+     reshapeOutput = tf.keras.layers.Reshape((reshapeSize, ))(tf.keras.layers.GlobalAveragePooling2D()(pretrainedLayers))
+     output = Dense(units=29, activation="softmax")(reshapeOutput)
+
+     #Getting the final model from the pretrained mobilenet model
+     mobile = tf.keras.Model(inputs = mobilenet.input, outputs = output)
+
+     #Fixing the first couple layers for training
+     for layer in mobile.layers[:-trainableLayerNumber]:
+         layer.trainable = False
+
+     #Compiling
+     mobile.compile(optimizer=Adam(1e-3), loss="categorical_crossentropy", metrics=['accuracy'])
+
+     #training model
+     mobile.fit(train_dataset, batch_size=batchSize, epochs=epochs)
+
+     # evaluating model
+     metrics = mobile.evaluate(test_dataset)
+     print(str(metrics))
+     fout.write(str(metrics) + "\n")
+
+     #Calculating the elapsed time for predicitons
+     #Making an initial prediction for the tf.function to initialize
+     mobile.predict(x=train_dataset[0][0])
+
+     start = time.time()
+     mobile.predict(x=train_dataset[1][0])
+     mobile.predict(x=train_dataset[2][0])
+     mobile.predict(x=train_dataset[3][0])
+     mobile.predict(x=train_dataset[4][0])
+     end = time.time()
+     print("Average time: " + str((end-start)/5) + " s\n")
+     fout.write("Average time: " + str((end-start)/5) + " s\n\n")
+
+     fout.close()
 
 fin.close()
-
-# #Getting the mobilenet model from the keras collections
-# mobilenet = tf.keras.applications.mobilenet.MobileNet()
-
-# #Switching last layers to a dense layer with the activation function for the asl dataset
-# switchLastLayers = 23
-# pretrainedLayers = mobilenet.layers[-switchLastLayers].output
-# reshapeOutput = tf.keras.layers.Reshape((512, ))(tf.keras.layers.GlobalAveragePooling2D()(pretrainedLayers))
-# output = Dense(units=29, activation="softmax")(reshapeOutput)
-
-# #Getting the final model from the pretrained mobilenet model
-# mobile = tf.keras.Model(inputs = mobilenet.input, outputs = output)
-
-# #Fixing the first couple layers for training
-# trainableLayerNumber = 1
-# for layer in mobile.layers[:-trainableLayerNumber]:
-#     layer.trainable = False
-
-
-# #Compiling
-# mobile.compile(optimizer=Adam(1e-3), loss="categorical_crossentropy", metrics=['accuracy'])
-
-# #training model
-# mobile.fit(train_dataset, batch_size=batchSize, epochs=epochs)
-
-# # evaluating model
-# mobile.evaluate(test_dataset)
-
-# #Calculating the elapsed time for predicitons
-# #Making an initial prediction for the tf.function to initialize
-# mobile.predict(x=train_dataset[0][0])
-
-# start = time.time()
-# mobile.predict(x=train_dataset[1][0])
-# mobile.predict(x=train_dataset[2][0])
-# mobile.predict(x=train_dataset[3][0])
-# mobile.predict(x=train_dataset[4][0])
-# end = time.time()
-# print("Average time: ", (end-start)/5, " s")
